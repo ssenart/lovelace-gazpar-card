94651c88ef1d088a23913ab921bc747cdf0eed11
Stéphane Senart <stephane.senart@gmail.com>
2022-10-12 23:13:16 +0200

    [#3] [Feature] Provide monthly history using a bar chart in addition to the table

--- dist/gazpar-card.js
+++ dist/gazpar-card.js
@@ -242,7 +242,7 @@ class GazparCard extends LitElement {
               ${this.config.showIcon
                 ? html`
                   <div class="icon-block">
-                    <span class="gazpar-icon bigger" style="background: none, url(/local/lovelace-gazpar-card/dist/gazpar-icon.png) no-repeat; background-size: contain;"></span>
+                    <span class="gazpar-icon bigger" style="background: none, url(/local/community/lovelace-gazpar-card/gazpar-icon.png) no-repeat; background-size: contain;"></span>
                   </div>`
                 : html `` 
               }
